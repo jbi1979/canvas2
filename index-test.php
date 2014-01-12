63f45f8ed19c2cc83a4ef912fcda9c9a96867c98
Bokin Jung <dhelper@naver.com>
2014-01-11 18:40:05 -0600

    git test

--- index-test.php
+++ index-test.php
@@ -4,7 +4,7 @@
 /**
  * This is the bootstrap file for test application.
  * This is the bootstrap file for test application.
- * This is the bootstrap file for test application.
+ * 111This is the bootstrap file for test application.
  * This file should be removed when the application is deployed for production.
  */
 
